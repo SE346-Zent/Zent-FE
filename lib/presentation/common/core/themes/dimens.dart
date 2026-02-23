cd74cc1993097c2cf5232c5bd65cd0a40aea2e4a
AnhGam <guardalex8@gmail.com>
2026-02-23 18:36:19 +0700

    chore: remove vscode settings from tracking

--- lib/presentation/common/core/themes/dimens.dart
+++ lib/presentation/common/core/themes/dimens.dart
@@ -7,8 +7,8 @@ class AppDimens {
   static const double spaceXl = 32.0;
 
   // Border Radius
-  static const double bora4 = 4.0;   // Checkboxes, Tags, Small Image Thumbnails
-  static const double bora8 = 8.0;   // Input fields, Secondary buttons, Alert Dialogs
-  static const double bora12 = 12.0; // Standards Cards, Bottom Sheets
-  static const double bora100 = 100.0; // Avatars, Status Chips
+  static const double boraxs = 4.0;   // Checkboxes, Tags, Small Image Thumbnails
+  static const double borasm = 8.0;   // Input fields, Secondary buttons, Alert Dialogs
+  static const double boramd = 12.0; // Standards Cards, Bottom Sheets
+  static const double boralg = 100.0; // Avatars, Status Chips
 }

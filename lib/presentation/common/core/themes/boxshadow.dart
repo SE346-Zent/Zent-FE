ca26c1ec4105abffd4d796dd19432f14f516c4a4
AnhGam <guardalex8@gmail.com>
2026-02-23 19:06:42 +0700

    chore: remove tracked vscode settings

--- lib/presentation/common/core/themes/boxshadow.dart
+++ lib/presentation/common/core/themes/boxshadow.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppBoxShadow {
+class BoxShadowStyles {
 
   static BoxShadow subtle = BoxShadow(
     color: const Color(0xFF000000).withValues(alpha: 0.05),

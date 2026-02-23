92baff040490506132226b89a103ae5f077b5525
AnhGam <guardalex8@gmail.com>
2026-02-23 18:46:40 +0700

    fix: textstyles follow GoogleFont format

--- lib/presentation/common/core/themes/text_styles.dart
+++ lib/presentation/common/core/themes/text_styles.dart
@@ -1,44 +1,38 @@
 import 'package:flutter/material.dart';
-
-class Typography {
+import 'package:google_fonts/google_fonts.dart';
+class TextStyles {
   
-  static const TextStyle display = TextStyle(
-    fontFamily: 'Lexend',
+  static final TextStyle display = GoogleFonts.lexend(
     fontWeight: FontWeight.w700, 
     fontSize: 32.0,
     height: 1.25, 
   );
 
-  static const TextStyle headline = TextStyle(
-    fontFamily: 'Lexend',
+  static final TextStyle headline = GoogleFonts.lexend(
     fontWeight: FontWeight.w600, 
     fontSize: 24.0,
     height: 1.33,
   );
 
-  static const TextStyle title = TextStyle(
-    fontFamily: 'Lexend',
+  static final TextStyle title = GoogleFonts.lexend(
     fontWeight: FontWeight.w500, 
     fontSize: 20.0,
     height: 1.40,
   );
 
-  static const TextStyle bodyLarge = TextStyle(
-    fontFamily: 'Source Sans 3',
+  static final TextStyle bodyLarge = GoogleFonts.sourceSans3(
     fontWeight: FontWeight.w400, 
     fontSize: 16.0,
     height: 1.50,
   );
 
-  static const TextStyle bodyMedium = TextStyle(
-    fontFamily: 'Source Sans 3',
+  static final TextStyle bodyMedium = GoogleFonts.sourceSans3(
     fontWeight: FontWeight.w400, 
     fontSize: 14.0,
     height: 1.50,
   );
 
-  static const TextStyle label = TextStyle(
-    fontFamily: 'Source Sans 3',
+  static final TextStyle label = GoogleFonts.sourceSans3(
     fontWeight: FontWeight.w500, 
     fontSize: 12.0,
     height: 1.33,

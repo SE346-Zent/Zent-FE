98b84d415aa78cc37e2334b972f1d5192484c75a
Dương Nguyễn Phú Quý <duongnguyenphuquy17052000@gmail.com>
2026-06-04 17:36:58 +0700

    fix FAB page

--- lib/presentation/common/core/layouts/admin_main_layout.dart
+++ lib/presentation/common/core/layouts/admin_main_layout.dart
@@ -80,7 +80,9 @@ class _AdminMainLayoutState extends State<AdminMainLayout> {
               child: Center(
                 child: _AnimatedFAB(
                   onTap: () {
-                    context.pushNamed(RouteNames.adminOperationalQueue);
+                    if (!isQueueScreen) {
+                      context.pushNamed(RouteNames.adminOperationalQueue);
+                    }
                   },
                 ),
               ),
